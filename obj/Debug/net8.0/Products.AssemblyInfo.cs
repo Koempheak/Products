52229d8998d658b38877f247dca317a706dfa9a7
Koempheak <139473134+Koempheak@users.noreply.github.com>
2024-03-22 10:12:33 -0500

    First commit

--- obj/Debug/net8.0/Products.AssemblyInfo.cs
+++ obj/Debug/net8.0/Products.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Products")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc9f91862c86e44005041e70613bfdd9cc029493")]
 [assembly: System.Reflection.AssemblyProductAttribute("Products")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Products")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
